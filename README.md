05b84b1afdabdc9d27ad0ab81d718fc7111bc9a8
JosephRidge <josephridge18@gmail.com>
2022-07-14 20:18:57 +0300

    Feature show image of items and toggle image type upon hover

--- README.md
+++ README.md
@@ -19,8 +19,36 @@ Allows  realtime uypdate / render upon change
 - has an extension of .vue
 - encanspulates / bundles together the logic, template and style into a single file to now what is known as a component. Hence the name `Single File Component`
 
-# Methods to use when authoring your Singl eFile component : 
+# Methods to use when authoring your Single File component : 
  - Options API 
 - Composition API
 
 # Create a new branch name it develop
+ `git branch develop`
+ - git add .
+ - git commit -m "New branch added " 
+ - git push
+
+# Project Details : 
+
+## Functionality : 
+->  Display Two items for sale ( DONE )
+->  Add feature to " Add to Cart " 
+->  Calculate the total price : "Total price " 
+->  Add more items to cart ( increment the item quantity plus one)
+-> Events ( mousehover and onclick)
+
+
+# Common Decalartives  : 
+preceded by the term "v-"
+- v-for // when having lists m, we can loop through the list and acces all elements using the v-for declarative
+
+- v-bind // allows one to bind logic/ JS features to your DOM Element
+
+- v-on // it checks for events and acts upon the defines or target event ->  you explicitly define your event eg. mouse-hover, click, scroll
+
+- v-if // conditional, use when you would like to ascertain a particular condition has been met
+
+- v-else-if // consditional decalarative that allows your to combine your expression / condition  eg islight === true && isPerson === false
+
+- v-show // conditional rendering, you can display an image only if a particular condition has been met  -> is more preferred approach as oppposed to the v-if 
---
+++ src/components/HomePage.vue
@@ -0,0 +1,90 @@
+<template>
+  <!-- Setting the Base Tag -->
+  <div id="myshop">
+    <!-- Image -->
+    <img :src="image" alt="Image of Nike T-shirt" />
+
+    <!-- info section -->
+    <!--  Within this info -->
+    <div id="infoSection">
+      <!-- Current Tshirt Colors -->
+
+      <ul v-for="(item, index) in items" :key="item.id">
+        <!-- {{ item.name }} : -->
+        <ul>
+            <!-- Event listening achieved !  -->
+          <div v-on:mouseover="changeImageOnHover(index)" :style="{ background: item.color }" id="colorCircle"></div>
+        </ul>
+      </ul>
+      <!-- Colors -->
+      <!-- Purchase Section -->
+      <h2>Info Section</h2>
+      <button>Add To Cart</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      image: "src/assets/red_nike_tshirt.png",
+      //     We have our items array here so what we are going to do it loop through them and display
+      items: [
+        {
+          id: 1,
+          name: "Red T-Shirt",
+          color: "red",
+          image: "src/assets/red_nike_tshirt.png",
+          price: 400,
+          currentStockAvailable: 10,
+        },
+        {
+          id: 2,
+          name: "Black T-Shirt",
+          color: "black",
+          image: "src/assets/black_nike_tshirt.jpg",
+          price: 400,
+          currentStockAvailable: 10,
+        },
+      ],
+    };
+  },
+ 
+  methods: {
+    //   Function is triggered on hover 
+     changeImageOnHover(index){
+         console.log("index", index)
+         this.image = this.items[index].image
+         return this.items[index].image
+
+    }
+  },
+};
+</script>
+
+<style>
+#myshop {
+  display: flex;
+}
+img {
+  height: 200px;
+  margin-right: 10px;
+}
+#infoSection {
+  height: 100%;
+  display: grid;
+  padding: 10px;
+}
+button {
+  background: #02afaf;
+}
+/* Lets change the cursor type on hover as well */
+#colorCircle {
+  width: 100px;
+  height: 20px;
+  border-radius: 30px;
+  cursor: pointer;
+  
+}
+</style>
